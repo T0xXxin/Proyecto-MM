1ed78c094219df6618befbb13134569a5ad509e9
T0xXxin <105995281+T0xXxin@users.noreply.github.com>
2022-05-21 17:10:30 -0500

    Add files via upload

---
+++ JS/funciones.js
@@ -0,0 +1,13 @@
+var slider = tns({
+    container: '.my-slider',
+    items: 3,
+    slideBy: 'page',
+    autoplay: true,
+    speed: 1000,
+    navAsThumbnails: true,
+    autoplayButtonOutput: false,
+    navPosition:'bottom',
+    controlsText: ["Anterior", "Siguiente"],
+    nav: true,
+    controlsPosition: 'bottom'
+  });
